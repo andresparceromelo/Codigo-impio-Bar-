c2c112ea61bc8e05d5a9c809ed17ed51c6f70114
andresparceromelo <131316669+andresparceromelo@users.noreply.github.com>
2024-10-08 09:11:42 -0500

    Update main.py

--- Trabajo codigo limpio/main.py
+++ Trabajo codigo limpio/main.py
@@ -20,15 +20,15 @@ def main():
     gestion_mesas.agregar_mesa(mesa2)
     gestion_mesas.agregar_mesa(mesa3)
 
-    mesero1 = Mesero("Carlos")
+    mesero1 = Mesero("Bedoya")
     mesero2 = Mesero("Ana")
 
     mesero1.mesas_asignadas.append(mesa1)
     mesero2.mesas_asignadas.append(mesa2)
 
-    admin = Administrador("Luis")
+    admin = Administrador("Jhonatan")
 
-    print("\nMesero Carlos realiza su trabajo:")
+    print("\nMesero Bedoya realiza su trabajo:")
     gestion_pedidos = GestionPedido()
     pedido1 = gestion_pedidos.crear_pedido()
     pedido1.agregar_producto(cerveza, 2)
@@ -37,7 +37,7 @@ def main():
     mesero1.liquidar_factura(pedido1, 10)
     mesero1.ver_mesas(gestion_mesas)
 
-    print("\nAdministrador Luis realiza su trabajo:")
+    print f "(\nAdministrador {admin} realiza su trabajo:)"
     admin.ver_mesas(gestion_mesas)
     admin.ver_propinas_meseros([mesero1, mesero2])
 
